fc8004d79f782c87dad2b73d756ded5c689dd76f
jinook929 <jinook929@gmail.com>
2021-01-20 16:49:50 -0500

    commit tree d9f2ae06a2c26f6423148d9e6c84d59e5e819365

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if !@roster.keys.include?(grade)
+            @roster[grade] = []
+            @roster[grade] << student
+        else
+            @roster[grade] << student
+        end
+    end
+
+    def grade(num)
+        @roster[num]
+    end
+
+    def sort
+        @sorted_roster = {}
+        @roster.sort.each {|grade, students|
+            @sorted_roster[grade] = students.sort
+        }
+        @sorted_roster
+    end
+end
